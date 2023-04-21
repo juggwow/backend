8c5b9871acee8d883747808ec92c76b20ab6470e
Juggwow <p.wiriyar@gmail.com>
2023-04-21 10:14:49 +0700

    config origin

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     methods: "GET,POST,PUT,DELETE",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 app.use("/auth", authRoute);
 
