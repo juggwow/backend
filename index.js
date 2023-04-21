acf1912021c0ef51a10994e587d0852d070e9d40
Juggwow <p.wiriyar@gmail.com>
2023-04-21 15:13:20 +0700

    config access cotrol 2

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ app.use(
   })
 );
 
-app.use(function(req,res,next){
-  res.header('Access-Control-Allow-Origin','*');
-  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers','Content-type');
-})
+// app.use(function(req,res,next){
+//   res.header('Access-Control-Allow-Origin','*');
+//   res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers','Content-type');
+// })
 
 app.use("/auth", authRoute);
 
